2ffe39f00e9bf6be534c8d189253a78e3bd642a9
Frederic Egmorte <frederic.egmorte@zama.ai>
2025-11-17 18:16:25 +0100

    feat: add coprocessor

---
+++ modules/coprocessor-infra/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.10"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.23"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.1"
+    }
+  }
+}
